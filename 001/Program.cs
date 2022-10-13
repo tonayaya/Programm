205916c7e4f26796939f4a33b5f1b6fe553f38ee
tonayaya <tonayaya@icloud.com>
2022-10-13 22:29:18 +0400

    001вывод текста

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Комментарий к коду (вывод строки на экран)
+Console.WriteLine("Hello, World1!");
+Console.WriteLine("Hello, World2!");
+Console.WriteLine("Hello, World3!");
+// различие WriteLine и Write - без перерноса
+Console.Write("Hello, World4!");
+Console.Write("Hello, World5!");
 Console.WriteLine("Hello, World!");
