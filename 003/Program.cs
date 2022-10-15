8bd3fc1993151456229146c65fedc0ec4ff0f0af
tonayaya <tonayaya@icloud.com>
2022-10-15 10:32:02 +0400

    oo3

--- 003/Program.cs
+++ 003/Program.cs
@@ -6,6 +6,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 
 //квадрат исходного числа
 int sqrNumber = number * number;
+//возведение в степень Math.Pow(number, 2)
 
 // вывод результат на экран
 Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
