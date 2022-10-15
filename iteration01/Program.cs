ba34873ff97a212fac4bffdc4ac51b04961ad927
tonayaya <tonayaya@icloud.com>
2022-10-15 11:17:45 +0400

    командная задача

---
+++ iteration01/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая принимает на вход два числа и вычитает не является ли одно квадратом другого.
+Console.WriteLine("Введите число №1: . . . ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число №2: . . . ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberA == numberB*numberB)
+{
+    Console.WriteLine($"правильно: число А = {numberA} является квадратом Б = {numberB}");
+}
+else
+{
+    Console.WriteLine($"ошибка: число А = {numberA} не является квадратом Б = {numberB}");
+}
---
+++ iteration02/Program.cs
@@ -0,0 +1,40 @@
+﻿// напишите программу, которая будет выдавать название дня недели по заданному номеру
+// 3 -> среда
+// 5 -> пятница
+
+Console.WriteLine("Введите число дня");
+int numberWeek = Convert.ToInt32(Console.ReadLine());
+if (numberWeek > 7 || numberWeek < 1)
+{
+    Console.WriteLine("Дней недели всего 7");
+}
+else
+{
+    if (numberWeek == 1)
+    {
+        Console.WriteLine("сегодня понедельник");
+    }
+}   if (numberWeek == 2)
+    {
+        Console.WriteLine("сегодня вторник");
+    }
+{if (numberWeek == 3)
+    {
+        Console.WriteLine("сегодня среда");
+    }
+}   if (numberWeek == 4)
+    {
+        Console.WriteLine("сегодня четверг");
+    }
+if (numberWeek == 5)
+    {
+        Console.WriteLine("сегодня пятница");
+    }
+if (numberWeek == 6)
+    {
+        Console.WriteLine("сегодня суббота");
+    }
+if (numberWeek == 7)
+    {
+        Console.WriteLine("сегодня воскресенье");
+}
